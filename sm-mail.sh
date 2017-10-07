cb23340bf43e70e516575abc9f3bf87c0316f2f9
kento tajiri <k.denjiry@gmail.com>
2017-10-07 12:05:31 +0900

    add rm tmp

--- sm-mail.sh
+++ sm-mail.sh
@@ -1,4 +1,5 @@
 FILE=/home/denjiry/Documents/HSIsparse/results/compdata_small.csv
 sort -r -t, -nk4 $FILE > sort.txt
 head -n 50 sort.txt|mail -s "SORT"  k.denjiry@gmail.com
+rm sort.txt
 tail -n 50 $FILE|mail -s "NOW"  k.denjiry@gmail.com
