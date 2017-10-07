6090cd9d654082561e71fdaecbcaa122b93e0c00
kento tajiri <k.denjiry@gmail.com>
2017-10-07 12:00:48 +0900

    add sm-mail.sh

---
+++ sm-mail.sh
@@ -0,0 +1,4 @@
+FILE=/home/denjiry/Documents/HSIsparse/results/compdata_small.csv
+sort -r -t, -nk4 $FILE > sort.txt
+head -n 50 sort.txt|mail -s "SORT"  k.denjiry@gmail.com
+tail -n 50 $FILE|mail -s "NOW"  k.denjiry@gmail.com
